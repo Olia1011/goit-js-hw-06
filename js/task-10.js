f958f0675cb89b33351c7f83b3493896986cd1fc
Olia1011 <olgamelnyk10111989@gmail.com>
2020-03-23 22:44:02 +0200

    last com

--- js/task-10.js
+++ js/task-10.js
@@ -3,16 +3,29 @@ import users from './users.js';
 
 const getSortedUniqueSkills = users =>
 users
-  .reduce((userSkills, user) => {
-    userSkills.push(...user.skills);
-    return userSkills;
-  }, [])
-  .reduce((sortedSkills, skill) => {
-    sortedSkills.some(item => skill === item) || sortedSkills.push(skill);
-    return sortedSkills;
+  .reduce((accum, user) => {
+    accum.push(...user.skills);
+    return accum;
   }, [])
   .sort();
 
-console.log(getSortedUniqueSkills(users));
-// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+  console.log(getSortedUniqueSkills(users));
+  // // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+    
+
+
+// const getSortedUniqueSkills = users =>
+// users
+//   .reduce((userSkills, user) => {
+//     userSkills.push(...user.skills);
+//     return userSkills;
+//   }, [])
+//   .reduce((sortedSkills, skill) => {
+//     sortedSkills.some(item => skill === item) || sortedSkills.push(skill);
+//     return sortedSkills;
+//   }, [])
+//   .sort();
+
+// console.log(getSortedUniqueSkills(users));
+// // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
   
